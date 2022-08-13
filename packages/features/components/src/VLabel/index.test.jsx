4a9c6a92af6c53786322aa5da71c028cbbbf20ec
AleTex <68469149+aletex1994@users.noreply.github.com>
2022-08-13 08:39:58 +0200

    Test/Component/VLabel

--- packages/features/components/src/VLabel/index.test.jsx
+++ packages/features/components/src/VLabel/index.test.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { render, fireEvent, waitFor, screen, toBeInTheDocument } from '../../../../../testUtils';
-import '@testing-library/jest-dom/extend-expect';
+import { render, fireEvent, waitFor, screen } from '../../../../../testUtils';
+import '@testing-library/jest-dom';
 import { VersionLabel } from '.';
 
 test('Check if the Version is displayed', () => {
