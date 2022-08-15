1b8634240cd31eddea649e09f5998859b926ac24
AleTex <68469149+aletex1994@users.noreply.github.com>
2022-08-15 17:09:54 +0200

    data.js

--- packages/features/components/src/TransactionModal/tests/data.js
+++ packages/features/components/src/TransactionModal/tests/data.js
@@ -15,5 +15,20 @@ module.exports = { 
       acc_principal_string : 'principalFrom',
       acc_id : 'idFrom',
       acc_extensible : 'no'
+    },
+    buyer_account : {
+      acc_principal_string : 'principalBuyer',
+      acc_id : 'idBuyer',
+      acc_extensible : 'no'
+    },
+    seller_account : {
+      acc_principal_string : 'principalSeller',
+      acc_id : 'idSeller',
+      acc_extensible : 'no'
+    },
+    dep_trx : {
+      _nat: '#nat',
+      _text: '#text',
+      _extensible:'#no',
     }
 }
