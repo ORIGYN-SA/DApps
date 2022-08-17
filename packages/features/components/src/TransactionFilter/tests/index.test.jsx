fa438da9ef0781adc7814255b3ff37a31338c3db
AleTex <68469149+aletex1994@users.noreply.github.com>
2022-08-17 11:53:22 +0200

    components/transactionsTable
    •created wrapper
    •took snapshot - match

--- packages/features/components/src/TransactionFilter/tests/index.test.jsx
+++ packages/features/components/src/TransactionFilter/tests/index.test.jsx
@@ -42,4 +42,5 @@ describe('Features > Component > TransactionFilter', () => {
         cleanup();
         }
     );
+
 });
--- packages/features/components/src/TransactionsTable/tests/index.test.jsx
+++ packages/features/components/src/TransactionsTable/tests/index.test.jsx
@@ -4,7 +4,49 @@ import '@testing-library/jest-dom';
 import testData from './data';
 
 import { TransactionsTable } from '..';
+const Wrapper = () => {
 
+  const myFilter ={
+    searchInputValue: '',
+    categoryToFilter: '',
+    transactionType: '',
+    update: 0
+  }
+  const myTrans={
+      trans_index: '0',
+      token_id: 'bm-1',
+      type_txn: 'Mint',
+      message: 'Mint',
+      accounts: ['92335d6272f9f32fcf5ca1586e1d8893581099c3436d5d1f59ab3751c6709282'],
+      principals: [],
+      mint_from: '6i6da-t3dfv-vteyg-v5agl-tpgrm-63p4y-t5nmm-gi7nl-o72zu-jd3sc-7qe',
+      mint_to: '6i6da-t3dfv-vteyg-v5agl-tpgrm-63p4y-t5nmm-gi7nl-o72zu-jd3sc-7qe',
+      sale: {
+        token: 'Token not defined',
+        amount: 'Amount not defined',
+      }
+  }
+  const [isLoading,setIsLoading] = useState(false);
+  const [indexID,setIndexID] = useState('');
+  const [filter, setFilter] = useState(myFilter);
+   const [transactionData, setTransactionData] = useState([myTrans]);
+   const [trans_types, setTrans_types] = React.useState([]);
+
+  return (
+    <TransactionsTable
+    setIsLoading={setIsLoading}
+    isLoading={isLoading}
+    searchBarTokenId={"myTOkenId"}
+    indexID={indexID}
+    setIndexID={setIndexID}
+    filter={filter}
+    setFilter={setFilter}
+    setTrans_types={setTrans_types}
+    setTransactionData={setTransactionData}
+    transactionData={transactionData}
+    />
+  );
+};
 describe('Components>TransactionsTable', () => {
   // Test1
   test('If is loading should display only preloader', () => {
@@ -54,5 +96,21 @@ describe('Components>TransactionsTable', () => {
     cleanup();
   } 
   );
-
+  // Test7
+  // the render of the wrapper should match the snapshot
+  test('Render', () => {
+    const { asFragment } = render(<Wrapper />);
+    expect(asFragment()).toMatchSnapshot();
+    cleanup();
+  }
+  );
+  // Test8
+  // the wrapper should contain the table
+  test('Table', () => {
+    const { asFragment } = render(<Wrapper />);
+    const Table = screen.getByLabelText('ogy_data_table');
+    expect(Table).toBeInTheDocument();
+    cleanup();
+  }
+  );
 });
