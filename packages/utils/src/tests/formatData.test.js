670f0078c91413d4ea51e784e7dfb4bc18de1549
AleTex <68469149+aletex1994@users.noreply.github.com>
2022-08-20 18:44:41 +0200

    FixedTests

--- packages/utils/src/tests/formatData.test.js
+++ packages/utils/src/tests/formatData.test.js
@@ -4,7 +4,8 @@ const bigIntString = '1519778444938790';
 const formattedData =  "18/01/1970 03:09pm";
 
 describe("utils > formatData", () => {
-  it('should return a formatted Data from a BIGINT string', () => {
-    expect(format_data(bigIntString)).toBe(formattedData);
-  });
+  //should return formatted data
+  it("should return formatted data", () => {
+    expect(format_data(bigIntString)).toEqual(formattedData);
+  } );
 });
