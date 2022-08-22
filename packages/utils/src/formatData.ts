da8b5973cfb740a3a1125fb8da5f9d8a1d1a0207
AleTex <68469149+aletex1994@users.noreply.github.com>
2022-08-22 12:04:20 +0200

    TestUpdated

--- packages/utils/src/formatData.ts
+++ packages/utils/src/formatData.ts
@@ -1,7 +1,7 @@
 import { padNum } from './number';
 import { formatTime } from './dateTime';
 
-export const format_data = (_date_int: string) => {
+export const format_data = (_date_int) => {
     const formatted_data = formatTime(BigInt(_date_int));
     return formatted_data;
   }
--- packages/utils/src/tests/formatData.test.js
+++ packages/utils/src/tests/formatData.test.js
@@ -1,11 +1,12 @@
 import { format_data } from "../formatData";
 
-const bigIntString = "1519778444938790";
+const bigIntNum = 1519778444938790;
 const formattedData =  "18/01/1970 03:09pm";
 
 describe("utils > formatData", () => {
-  //should return formatted data
-  it("should return formatted data", () => {
-    expect(format_data(bigIntString)).toMatch(formattedData);
-  } );
+  //should return a string with the date formatted
+  it("should return a string with the date formatted", () => {
+    expect(format_data(bigIntNum)).toEqual(formattedData);
+  }
+  );
 });
