ae4c871e17119d5c74b70261eef161e928aeec3c
AleTex <68469149+aletex1994@users.noreply.github.com>
2022-08-22 12:08:48 +0200

    DateTime

--- packages/utils/src/tests/formatData.test.js
+++ packages/utils/src/tests/formatData.test.js
@@ -1,12 +1,12 @@
 import { format_data } from "../formatData";
 
-const bigIntNum = 1519778444938790;
-const formattedData =  "18/01/1970 03:09pm";
+const bigIntString = "1519778444938790";
+const formattedData =  "18/01/1970 02:09pm";
 
 describe("utils > formatData", () => {
   //should return a string with the date formatted
   it("should return a string with the date formatted", () => {
-    expect(format_data(bigIntNum)).toEqual(formattedData);
+    expect(format_data(bigIntString)).toEqual(formattedData);
   }
   );
 });
