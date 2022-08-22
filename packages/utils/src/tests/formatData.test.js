05d5409fca27adde7c58f691f07ae5ab39be53f4
AleTex <68469149+aletex1994@users.noreply.github.com>
2022-08-22 11:49:32 +0200

    Update formatData.test.js

--- packages/utils/src/tests/formatData.test.js
+++ packages/utils/src/tests/formatData.test.js
@@ -1,11 +1,11 @@
 import { format_data } from "../formatData";
 
-const bigIntString = '1519778444938790';
+const bigIntString = "1519778444938790";
 const formattedData =  "18/01/1970 03:09pm";
 
 describe("utils > formatData", () => {
   //should return formatted data
   it("should return formatted data", () => {
-    expect(format_data(bigIntString)).toEqual(formattedData);
+    expect(format_data(bigIntString)).toMatch(formattedData);
   } );
 });
