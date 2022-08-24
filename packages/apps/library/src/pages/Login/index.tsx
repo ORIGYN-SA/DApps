18808a939496f1f69ed74f0702bfbe8e0549d09d
Frank Andreas <andreass.frankk@gmail.com>
2022-08-24 12:44:58 +0300

    added Treeview

--- packages/apps/library/src/pages/Login/index.tsx
+++ packages/apps/library/src/pages/Login/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Box from '@mui/material/Box';
 import LibraryAccordion from "../../components/LibraryAccordion";
+import Treeview from "../../components/Treeview";
 
 const Library = () => {
 
   return (  
         <Box
           sx={{ m: 2,p: 2}}>
-            <LibraryAccordion/>     
+            <LibraryAccordion/>
+            <Treeview/>     
         </Box>
   );
 };
