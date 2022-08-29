6af70b144e50f63f715d0a47e56d7d2baa8dc63e
Frank Andreas <andreass.frankk@gmail.com>
2022-08-29 10:21:07 +0300

    Update index.tsx

--- packages/apps/library/src/components/TreeviewPart/index.tsx
+++ packages/apps/library/src/components/TreeviewPart/index.tsx
@@ -7,31 +7,31 @@ import TreeItem from '@mui/lab/TreeItem';
 
 const TreeViewPart = ({ children }: any) => {
   const { actor, principal, canisterId } = useContext(AuthContext);
-  // const [libData, setLibData] = useState([]);
+  const [libData, setLibData] = useState([]);
   const [nftData, setNftData] = useState([]);
-
-  // useEffect(() => {
-  //   if (actor) {
-  //     actor
-  //       .nft_origyn(tokenId)
-  //       .then((r) => {
-  //         setLibData(
-  //           r.ok.metadata.Class.filter((res) => {
-  //             return res.name === 'library';
-  //           })[0].value.Array.thawed,
-  //         );
-  //         console.log('nft_origyn', r);
-  //       })
-  //       .catch(console.log);
-  //   }
-  // }, [actor]);
 
   useEffect(() => {
     if (actor) {
       actor
-        .collection_nft_origyn({principal})
+        .balance_of_nft_origyn("6i6da-t3dfv-vteyg-v5agl-tpgrm-63p4y-t5nmm-gi7nl-o72zu-jd3sc-7qe")
         .then((r) => {
-          setNftData(r.ok.nfts)
+          setLibData(
+            r.ok.metadata.Class.filter((res) => {
+              return res.name === 'library';
+            })[0].value.Array.thawed,
+          );
+          console.log('nft_origyn', r);
+        })
+        .catch(console.log);
+    }
+  }, [actor]);
+
+  useEffect(() => {
+    if (actor) {
+      actor
+        .collection_nft_origyn({ principal })
+        .then((r) => {
+          setNftData(r.ok.nfts);
           console.log('balance_of_nft_origyn', r);
         })
         .catch(console.log);
@@ -44,20 +44,19 @@ const TreeViewPart = ({ children }: any) => {
         aria-label="file system navigator"
         defaultCollapseIcon={<ExpandMoreIcon />}
         defaultExpandIcon={<ChevronRightIcon />}
-        
       >
-        <TreeItem nodeId="1" label="NFTs">
-          {nftData?.map((nft) => (
-            <TreeItem
-              key={nft}
-              nodeId={nft}
-              label={nft}
-            >
-            </TreeItem>
-          ))}
-        </TreeItem>
-        <TreeItem nodeId="2" label="Library">
-        <TreeItem nodeId="3" label={children} />
+        <TreeItem nodeId="0" label={canisterId}>
+          <TreeItem nodeId="1" label="NFTs">
+            {nftData?.map((nft) => (
+              // AICI TREBUIE SA-I ADAUG UN PREVIEW LA NFT, APOI SA COBOR IAR CU LIBRARII IN TREEVIEW
+              <TreeItem key={nft} nodeId={nft} label={nft.name}>
+                <TreeItem nodeId={nft} label={nft.library}/>
+              </TreeItem>
+            ))}
+          </TreeItem>
+          <TreeItem nodeId="2" label="Library">
+            <TreeItem nodeId="3" label={children} />
+          </TreeItem>
         </TreeItem>
       </TreeView>
     </div>
