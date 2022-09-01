602e6e39cc2848f5db28b9ddc4cc7dc05c8f128c
Frank <75906369+frank0170@users.noreply.github.com>
2022-09-01 15:59:19 +0300

    Update index.tsx

--- packages/apps/library/src/components/NFTBox/index.tsx
+++ packages/apps/library/src/components/NFTBox/index.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Card, CardContent, Typography, Box } from '@mui/material';
+import { AuthContext } from '@dapp/features-authentication';
 
 interface NFTDATA {
   nft_id: string;
-  preview: string;
+  preview?: string;
 }
 
 const NFTBox = (props: any) => {
+  const { actor } = useContext(AuthContext);
+  const { nftStuff, setNftStuff } = useState({});
+
   let nftData: NFTDATA = {
-    nft_id: props.currentNft?.Class[0]?.value?.Text,
-    preview: props.currentNft?.Class[1]?.value?.Text,
+    nft_id: props.currentNft,
   };
+
+  const nftDetails = async () => {
+    const details = await actor?.nft_origyn(nftData.nft_id);
+    console.log('here me', details);
+    return setNftStuff(details);
+  };
+
+  useEffect(() => {
+    if (actor) {
+      nftDetails();
+    }
+  }, [actor]);
+
+  const nftImage = nftStuff?.ok?.metadata?.Class.filter((res) => {
+    return res.name === 'primary_asset';
+  }).value.Text;
 
   return (
     <div>
@@ -27,7 +46,7 @@ const NFTBox = (props: any) => {
             NFT ID: <b> {nftData.nft_id} </b>
           </Typography>
           <Box>
-            <img src={nftData.preview} height="300px"></img>
+            <img src={nftImage} height="300px"></img>
           </Box>
         </CardContent>
       </Card>
