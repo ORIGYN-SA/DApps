47aff4d0a0a0d80a70a27aaf57a493890d9d82cb
AleTex <68469149+aletex1994@users.noreply.github.com>
2022-10-12 18:54:51 +0200

    Update index.tsx

--- packages/features/components/src/VLabel/index.tsx
+++ packages/features/components/src/VLabel/index.tsx
@@ -4,7 +4,6 @@ import { Typography } from '@mui/material';
 
 export const VersionLabel = (props : any) => {
   const VNumber: string = props.ledgerVersion;
-
   return (
     <Typography
       variant="button"
