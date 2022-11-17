009b087e35da6daa575c7293e50d7c4199de9f7a
AleTex <68469149+aletex1994@users.noreply.github.com>
2022-11-17 10:37:05 +0100

    NftData - Fixes

--- packages/apps/nft-data/src/pages/NFTInfo/index.tsx
+++ packages/apps/nft-data/src/pages/NFTInfo/index.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Tab from '@mui/material/Tab';
-
 import Tabs from '@mui/material/Tabs';
 import TreeTab from '../../components/nftTabs/TreeTab';
 import RawTab from '../../components/nftTabs/RawTab';
 import FormTab from '../../components/nftTabs/FormTab';
-import NewForm from '../../components/nftTabs/NewForm';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import JSONTab from '../../components/nftTabs/JSONTab';
@@ -43,8 +41,7 @@ const NFTInfo = ({ metadata }: any) => {
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
-
-<<<<<<< HEAD
+  return (
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
@@ -71,43 +68,7 @@ const NFTInfo = ({ metadata }: any) => {
         <NewForm metadata={metadata} />
       </TabPanel>
     </Box>
-      </Container>
-    );
+  );
   };
   
   export default NFTInfo;
-=======
-  return (
-    <Container maxWidth="xl">
-      <Box sx={{ width: '100%' }}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-            <Tab label="Form" />
-            <Tab label="Tree" />
-            <Tab label="Raw" />
-            <Tab label="JSON" />
-            <Tab label="NEW FORM" />
-          </Tabs>
-        </Box>
-        <TabPanel value={value} index={0}>
-          <FormTab metadata={metadata} />
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          <TreeTab metadata={metadata} />
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          <RawTab metadata={metadata} />
-        </TabPanel>
-        <TabPanel value={value} index={3}>
-          <JSONTab metadata={metadata} />
-        </TabPanel>
-        <TabPanel value={value} index={4}>
-          <NewForm metadata={metadata} />
-        </TabPanel>
-      </Box>
-    </Container>
-  );
-};
-
-export default NFTInfo;
->>>>>>> main
--- packages/apps/nft-data/webpack.config.js
+++ packages/apps/nft-data/webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('../../../webpack.config');
 
 const appConfig = {
   name: 'nftData',
-  openPage: '-/bmdev/-/bm-1/-/nftData';
+  openPage: '-/bmdev/-/bm-1/-/nftData',
 };
 module.exports = (env, argv) => {
   const res = webpack(env, argv, appConfig);
