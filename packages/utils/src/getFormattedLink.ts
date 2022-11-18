881f7cad6e09f3dc6996df2911c9fc0ca58a3af1
AleTex <68469149+aletex1994@users.noreply.github.com>
2022-11-18 15:19:35 +0100

    Update getFormattedLink.ts
    Fixed case blob - localhost

--- packages/utils/src/getFormattedLink.ts
+++ packages/utils/src/getFormattedLink.ts
@@ -26,23 +26,29 @@ export const GetFormattedLink = async (currCanisterId, linkToFormat) => {
 
     const QueryName: any = await phonebookActor?.reverse_lookup(Principal.fromText(currCanisterId));
     const HasRoot = linkToFormat.includes(currCanisterId || QueryName);
-
+    const HasLocalHost = linkToFormat.includes('localhost');
+    const HasBlob = linkToFormat.includes('blob:');
     let formattedLink: string = '';
     try {
-        if (await QueryName == "" || await QueryName == null) {
-            formattedLink = (HasRoot) ?
-                (linkToFormat) : ('https://' + currCanisterId + '.raw.ic0.app/' + linkToFormat);
-                return formattedLink;
+        if (HasLocalHost || HasBlob) {
+            formattedLink = linkToFormat;
+            return formattedLink;
         } else {
-            if(HasRoot) {
-                let NewLink = new URL(linkToFormat);
-                let HostString = NewLink.hostname;
-                let PrptlLink = linkToFormat.replace(HostString, 'prptl.io/-/' + QueryName);
-                formattedLink = PrptlLink;
+            if (await QueryName == "" || await QueryName == null) {
+                formattedLink = (HasRoot) ?
+                    (linkToFormat) : ('https://' + currCanisterId + '.raw.ic0.app/' + linkToFormat);
                 return formattedLink;
-            }else{
-                formattedLink = 'https://prptl.io/-/' + QueryName +'/-/'+ linkToFormat;
-                return formattedLink;
+            } else {
+                if (HasRoot) {
+                    let NewLink = new URL(linkToFormat);
+                    let HostString = NewLink.hostname;
+                    let PrptlLink = linkToFormat.replace(HostString, 'prptl.io/-/' + QueryName);
+                    formattedLink = PrptlLink;
+                    return formattedLink;
+                } else {
+                    formattedLink = 'https://prptl.io/-/' + QueryName + '/-/' + linkToFormat;
+                    return formattedLink;
+                }
             }
         }
     } catch (e) {
