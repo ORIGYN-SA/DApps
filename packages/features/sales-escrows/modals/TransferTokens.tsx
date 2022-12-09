d0839e7c6e5529e8d3f5529fa524276af12bc57b
Frank <andreass.frankk@gmail.com>
2022-12-09 17:23:52 +0200

    added Transfer Tokens

--- packages/features/sales-escrows/modals/TransferTokens.tsx
+++ packages/features/sales-escrows/modals/TransferTokens.tsx
@@ -1,165 +1,110 @@
 import React, { useContext, useEffect, useState } from 'react';
-import Button from '@mui/material/Button';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { sendTransaction, useTokensContext } from '@dapp/features-tokens-provider';
-import { DateTimePicker } from '@mui/x-date-pickers';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
-import { TokenIcon } from '@dapp/features-components';
 import { AuthContext } from '@dapp/features-authentication';
-import { Container, Flex, HR, Modal, TextInput, Select } from '@origyn-sa/origyn-art-ui';
+import { Container, Flex, HR, Modal, TextInput, Select, Button } from '@origyn-sa/origyn-art-ui';
 
 const TransferTokensModal = ({ currentToken, open, handleClose }: any) => {
   const { actor, ogyActor, principal } = React.useContext(AuthContext);
   const [isLoading, setIsLoading] = React.useState(false);
   const [token, setToken] = React.useState('OGY');
   const { tokens, refreshAllBalances } = useTokensContext();
-  const [selectedToken, setSelectedToken] = useState();
-  const [amount, setAmount] = useState<any>(0)
-  const [ receiver, setReceiver] = useState<any>()
-  const [memo, setMemo] = useState<any>()
+  const [selectedToken, setSelectedToken] = useState<any>('OGY');
+  const [amount, setAmount] = useState<any>(0);
+  const [receiver, setReceiver] = useState<any>();
+  const [memo, setMemo] = useState<any>();
+  const walletType = localStorage.getItem('loggedIn');
+  const [switchTransfer, setSwitchTransfer] = useState(false);
 
-  // const amount = 1 * 1e8;
-  // const walletType = localStorage.getItem('loggedIn');
-  // const saleInfo = await actor.sale_info_nft_origyn({ deposit_info: [] });
+  const sendTrx = () => {
+    sendTransaction(walletType, tokens[selectedToken], receiver, amount);
+    setSwitchTransfer(true);
+  };
 
-  // const { account_id } = saleInfo?.ok?.deposit_info ?? {};
+  //   <Container size='full' padding='48px'>
+  //             <h2>Success!</h2>
+  //             <br/>
+  //             <span>Your transfer of {amount} {selectedToken} in complete.
+  //             Click done to return to the dashboard.</span>
+  //             <br/>
+  //             <Button btnType="secondary" onClick={() => handleClose(false)}>Done</Button>
 
-  // const transactionHeight = await sendTransaction(
-  //   walletType,
-  //   tokens[token],
-  //   new Uint8Array(account_id),
-  //   amount,
-  // );
+  //        </Container>
 
   return (
     <div>
       <Modal isOpened={open} closeModal={() => handleClose(false)} size="md">
-        <Container size="full" padding="48px">
-          <h2>Transfer Tokens</h2>
-          <br />
-          <h3> You can only send OGY from your available balance.</h3>
-          <Flex flexFlow="column" gap={8}>
+        {switchTransfer ? (
+          <Container size="full" padding="48px">
+            <h2>Transfer in Progress</h2>
             <br />
-            <Select
-              label="Select token"
-              handleChange={(option) => {
-                setSelectedToken(option.value);
-              }}
-              options={Object.keys(tokens).map((standard) => ({
-                value: standard,
-                label: standard,
-              }))}
-            />
-            <br/>
-            <Flex flexFlow='row' justify="space-between">
+            <span>loading bar placeholder</span>
+          </Container>
+        ) : (
+          <Container size="full" padding="48px">
+            <h2>Transfer Tokens</h2>
+            <br />
+            <span> You can only send OGY from your available balance.</span>
+            <Flex flexFlow="column" gap={8}>
+              <br />
+              <span>Select token</span>
+              <Select
+                placeholder="OGY"
+                handleChange={(option) => {
+                  setSelectedToken(option.value);
+                }}
+                options={Object.keys(tokens).map((standard) => ({
+                  value: standard,
+                  label: standard,
+                }))}
+              />
+              <br />
+              <Flex flexFlow="row" justify="space-between">
                 <span>Amount</span>
-                <span>500 </span>
-                </Flex>
-                <TextInput
-                  id='standard-helperText'
-                  onChange={(e) => setAmount(e.target.value)}
-                  required
-                />
-                <br/>
-            <TextInput
-                  id='standard-helperText'
-                  label='Recipient Address'
-                  onChange={(e) => setReceiver(e.target.value)}
-                  required
-                />
-                <br/>
-            <TextInput
-                  id='standard-helperText'
-                  label='Memo'
-                  onChange={(e) => setMemo(e.target.value)}
-                  required
-                />
-          </Flex>
-          <br/>
-          <h3>Transaction Fee</h3>
-          <span>0.002 OGY</span>
-          <br/>
-          <br/>
-          <h3>Total Amount</h3>
-        </Container>
+                <span>{tokens[selectedToken].balance}</span>
+              </Flex>
+              <TextInput
+                id="standard-helperText"
+                onChange={(e) => setAmount(e.target.value)}
+                required
+              />
+              <br />
+              <span>Recipient Address</span>
+              <TextInput
+                id="standard-helperText"
+                onChange={(e) => setReceiver(e.target.value)}
+                required
+              />
+              <br />
+              <span>Memo</span>
+              <TextInput
+                id="standard-helperText"
+                onChange={(e) => setMemo(e.target.value)}
+                required
+              />
+              <br />
+              <span>Transaction Fee</span>
+              <span>0.002 OGY</span>
+              <br />
+            </Flex>
+            <HR color="DARK_GREY" />
+            <br />
+            <Flex flexFlow="row" justify="space-between">
+              <h3>Total Amount</h3>
+              <span>{Number(amount) + 0.002}</span>
+            </Flex>
+            <br />
+            <HR color="DARK_GREY" />
+            <br />
+            <Flex justify="flex-end">
+              <Button btnType="secondary" onClick={sendTrx}>
+                Transfer OGY
+              </Button>
+            </Flex>
+          </Container>
+        )}
       </Modal>
     </div>
   );
 };
-
-// (<div>
-//     <Modal
-//      isOpened={open}
-//       closeModal={() => handleClose(false)}
-//       size="md"
-//     >
-//       <Container size='full' padding='48px'>
-//         <h2>Transfer Tokens</h2>
-//         <br/>
-//         <Flex flexFlow="column" gap={8}>
-//           <TextInput
-//             required
-//             label="Start Price"
-//             id="startPrice"
-//             value={startPrice}
-//             onChange={(e) => setStartPrice(e.target.value)}
-//             error={errors?.startPrice?.message as string || ""}
-//           />
-//           <TextInput
-//             required
-//             label="Min Increase"
-//             id="minIncrease"
-//             value={priceStep}
-//             onChange={(e) => setPriceStep(e.target.value)}
-//             error={errors?.minIncrease?.message as string || ""}
-//           />
-//           <TextInput
-//             required
-//             label="Buy Now Price"
-//             id="outlined-basic"
-//             value={buyNowPrice}
-//             onChange={(e) => setBuyNowPrice(e.target.value)}
-//             error={errors?.buyNowPrice?.message as string || ""}
-//           />
-//           <LocalizationProvider fullWidth dateAdapter={AdapterMoment}>
-//             <DateTimePicker
-//               label="End Date"
-//               value={endDate}
-//               onChange={(newValue) => {
-//                 setEndDate(newValue);
-//               }}
-//               renderInput={(params) => (
-//                 <TextField {...params} {...register('endDate')} error={!!errors.endDate} />
-//               )}
-//             />
-//           </LocalizationProvider>
-//           <FormControl fullWidth>
-//             <InputLabel id="demo-simple-select-label">Token</InputLabel>
-//             <Select
-//               labelId="demo-simple-select-label"
-//               id="demo-simple-select"
-//               value={token}
-//               label="Token"
-//               onChange={handleChange}
-//             >
-//               {Object.keys(tokens).map((t, index) => (
-//                 <MenuItem key={`${token}+${index}`} value={t}>
-//                   <TokenIcon symbol={tokens[t].icon} />
-//                   {tokens[t].symbol}
-//                 </MenuItem>
-//               ))}
-//             </Select>
-//           </FormControl>
-//           <HR color='MID_GREY'/>
-//           <Flex>
-//             <Button onClick={() => handleClose(false)}>Cancel</Button>
-//             <Button onClick={handleSubmit(customSubmit)} autoFocus>
-//               Start
-//             </Button>
-//           </Flex>
-//         </Flex>
-//       </Container>
-//     </Modal>
-//   </div>)
 
 export default TransferTokensModal;
