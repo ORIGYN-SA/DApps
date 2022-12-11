e78157a62af3b9c30ccf9e29e99cc3f9c2406013
Sebastian Deme <deme.sebastian@gmail.com>
2022-12-11 18:46:34 +0200

    SI-212 build: upload dApps build to S3

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,12 @@ module.exports = {
   moduleNameMapper: {
     '\\.(scss|svg|css)$': '<rootDir>/testUtils/fileMock.js',
     '@testUtils': '<rootDir>/testUtils/index.js',
+    '@connect2ic/react': '<rootDir>/node_modules/@connect2ic/react',
+    '@connect2ic/core': '<rootDir>/node_modules/@connect2ic/core',
   },
+
   moduleDirectories: ['node_modules', 'packages', 'src'],
   testEnvironment: '<rootDir>/testUtils/testEnv.js',
-  transformIgnorePatterns: ['node_modules/(?!@dfinity)'],
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest',
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
