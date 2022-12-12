5a4700e976963501cb163709304656ec63748b9e
Frank <andreass.frankk@gmail.com>
2022-12-12 19:16:47 +0200

    Update StartAuctionModal.tsx

--- packages/features/sales-escrows/modals/StartAuctionModal.tsx
+++ packages/features/sales-escrows/modals/StartAuctionModal.tsx
@@ -1,47 +1,53 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import { Principal } from '@dfinity/principal';
-import { AuthContext } from '@dapp/features-authentication';
-import {
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  TextField,
-} from '@mui/material';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { useSnackbar } from 'notistack';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as Yup from 'yup';
-import { DateTimePicker } from '@mui/x-date-pickers';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
-import { TokenIcon } from '@dapp/features-components';
-import { useTokensContext } from '@dapp/features-tokens-provider';
-import { Container, Flex, HR, Modal, TextInput } from '@origyn-sa/origyn-art-ui'
+import * as React from 'react'
+import { Principal } from '@dfinity/principal'
+import { AuthContext } from '@dapp/features-authentication'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { useSnackbar } from 'notistack'
+import * as Yup from 'yup'
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
+import { useTokensContext } from '@dapp/features-tokens-provider'
+import { Container, Flex, HR, Modal, TextInput, DatePicker, Select, Button } from '@origyn-sa/origyn-art-ui'
+
+const validationSchema = Yup.object({
+  token: Yup.string()
+    .default("OGY"),
+  startPrice: Yup.number()
+    .typeError('This must be a number')
+    .nullable()
+    .typeError('This cannot be a nullable number')
+    .required('Start Price is required')
+    .default(0),
+  minIncrease: Yup.number()
+    .typeError('This must be a number')
+    .nullable()
+    .required('Minimum increase step is required')
+    .default(0),
+  buyNowPrice: Yup.number()
+    .typeError('This must be a number')
+    .nullable()
+    .typeError('This cannot be a nullable number')
+    .moreThan(Yup.ref('startPrice'), 'Instant buy price must be greater than the start price')
+    .default(0),
+  endDate: Yup.date().min(new Date(), 'The end date needs to be in the future!').default(new Date()),
+})
 
 export function StartAuctionModal({ currentToken, open, handleClose }: any) {
-  const { actor } = React.useContext(AuthContext);
-  const { enqueueSnackbar } = useSnackbar();
-  const [token, setToken] = React.useState('OGY');
-  const [inProgress, setInProgress] = React.useState(false);
-  const [buyNowPrice, setBuyNowPrice] = React.useState<any>();
-  const [startPrice, setStartPrice] = React.useState<any>();
-  const [priceStep, setPriceStep] = React.useState<any>();
-  const [endDate, setEndDate] = React.useState<any>(new Date().toJSON().slice(0, 19));
-  const { tokens } = useTokensContext();
-
-  const handleChange = (event) => {
-    setToken(event.target.value as string);
-  };
+  const { actor } = React.useContext(AuthContext)
+  const { enqueueSnackbar } = useSnackbar()
+  const [errors, setErrors] = React.useState<any>({})
+  // @ts-ignore
+  const [values, setValues] = React.useState<any>(validationSchema.default());
+  const [inProgress, setInProgress] = React.useState(false)
+  const { tokens } = useTokensContext()
 
   const handleStartAuction = async ({
-    startPrice,
-    buyNowPrice,
-    minIncrease: priceStep,
-    endDate,
-  }) => {
-    setInProgress(true);
+                                      startPrice,
+                                      buyNowPrice,
+                                      minIncrease: priceStep,
+                                      endDate,
+                                      token: saleToken
+                                    }) => {
+    setInProgress(true)
     try {
       const resp = await actor.market_transfer_nft_origyn({
         token_id: currentToken?.Class?.find(({ name }) => name === 'id').value.Text,
@@ -51,11 +57,11 @@ export function StartAuctionModal({ currentToken, open, handleClose }: any) {
               start_price: BigInt(startPrice * 1e8),
               token: {
                 ic: {
-                  fee: BigInt(tokens[token]?.fee ?? 200000),
-                  decimals: BigInt(tokens[token]?.decimals ?? 8),
-                  canister: Principal.fromText(tokens[token]?.canisterId),
+                  fee: BigInt(tokens[saleToken]?.fee ?? 200000),
+                  decimals: BigInt(tokens[saleToken]?.decimals ?? 8),
+                  canister: Principal.fromText(tokens[saleToken]?.canisterId),
                   standard: { Ledger: null },
-                  symbol: tokens[token]?.symbol,
+                  symbol: tokens[saleToken]?.symbol,
                 },
               },
               reserve: [],
@@ -67,14 +73,14 @@ export function StartAuctionModal({ currentToken, open, handleClose }: any) {
               buy_now: [BigInt(buyNowPrice * 1e8)],
               // end_date: BigInt(new Date(endDate).getTime() * 1e6), TODO: figure this out
               ending: {
-                date: BigInt(new Date(endDate).getTime() * 1e6),
+                date: BigInt(endDate.getTime() * 1e6),
               },
             },
           },
           broker_id: [],
           escrow_receipt: [],
         },
-      });
+      })
 
       if ('err' in resp) {
         enqueueSnackbar('There was an error when starting your auction.', {
@@ -83,7 +89,7 @@ export function StartAuctionModal({ currentToken, open, handleClose }: any) {
             vertical: 'top',
             horizontal: 'right',
           },
-        });
+        })
       } else {
         enqueueSnackbar('Your auction has been started.', {
           variant: 'success',
@@ -91,113 +97,109 @@ export function StartAuctionModal({ currentToken, open, handleClose }: any) {
             vertical: 'top',
             horizontal: 'right',
           },
-        });
-        handleClose(true);
+        })
+        handleClose(true)
       }
     } catch (e) {
-      console.log(e);
+      console.log(e)
       enqueueSnackbar('There was an error when starting your auction.', {
         variant: 'error',
         anchorOrigin: {
           vertical: 'top',
           horizontal: 'right',
         },
-      });
+      })
     }
-    setInProgress(false);
-  };
-  const validationSchema = Yup.object().shape({
-    startPrice: Yup.number()
-      .typeError('This must be a number')
-      .nullable()
-      .typeError('This cannot be a nullable number'),
-    minIncrease: Yup.string().required('Minimum increase step is required'),
-    buyNowPrice: Yup.number()
-      .typeError('This must be a number')
-      .nullable()
-      .typeError('This cannot be a nullable number')
-      .moreThan(Yup.ref('startPrice'), 'Instant buy price must be greater than the start price'),
-    endDate: Yup.date().min(new Date(), 'The end date needs to be in the future!'),
-  });
+    setInProgress(false)
+  }
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(validationSchema),
-  });
-  const customSubmit = (data) => {
-    handleStartAuction(data);
-  };
+  const getValidationErrors = (err) => {
+    const validationErrors = {}
+
+    err.inner.forEach(error => {
+      if (error.path) {
+        validationErrors[error.path] = error.message
+      }
+    })
+
+    return validationErrors
+  }
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    validationSchema.validate(values, { abortEarly: false }).then((v) => {
+      console.log(values, values.endDate.getTime())
+      handleStartAuction(values);
+    })
+      .catch(function(e) {
+        const errs = getValidationErrors(e);
+        setErrors(errs)
+      })
+  }
+  const onChange = (e?: any, name?: string, value?: any) => {
+    setErrors({...errors, [name || e.target.name]: undefined});
+    setValues({...values, [name || e.target.name]: value || e.target.value});
+  }
+  console.log()
   return (
     <div>
       <Modal
         isOpened={open}
         closeModal={() => handleClose(false)}
-        size="md"
+        size='md'
       >
         <Container size='full' padding='48px'>
           <h2>Start an Auction</h2>
-          <br/>
-          <Flex flexFlow="column" gap={8}>
-            <TextInput
-              required
-              label="Start Price"
-              id="startPrice"
-              value={startPrice}
-              onChange={(e) => setStartPrice(e.target.value)}
-              error={errors?.startPrice?.message as string || ""}
+          <br />
+          <Flex as='form' onSubmit={handleSubmit} action='' flexFlow='column' gap={8}>
+            <Select
+              label='Token'
+              name='token'
+              /*@ts-ignore*/
+              selectedOption={{ label: values.token, value: values.token }}
+              handleChange={(v) => onChange(null, 'token', v.value)}
+              options={Object.keys(tokens).map((t) => ({ label: tokens[t].symbol, value: t }))}
             />
             <TextInput
               required
-              label="Min Increase"
-              id="minIncrease"
-              value={priceStep}
-              onChange={(e) => setPriceStep(e.target.value)}
-              error={errors?.minIncrease?.message as string || ""}
+              label='Starting Price'
+              name='startPrice'
+              type='number'
+              value={values.startPrice}
+              onChange={onChange}
+              error={errors?.startPrice}
             />
             <TextInput
               required
-              label="Buy Now Price"
-              id="outlined-basic"
-              value={buyNowPrice}
-              onChange={(e) => setBuyNowPrice(e.target.value)}
-              error={errors?.buyNowPrice?.message as string || ""}
+              label='Buy Now Price'
+              name='buyNowPrice'
+              type='number'
+              value={values.buyNowPrice}
+              onChange={onChange}
+              error={errors?.buyNowPrice}
+            />
+            <TextInput
+              required
+              type='number'
+              label='Minimum Increase'
+              name='minIncrease'
+              value={values.minIncrease}
+              onChange={onChange}
+              error={errors?.minIncrease}
             />
             <LocalizationProvider fullWidth dateAdapter={AdapterMoment}>
-              <DateTimePicker
-                label="End Date"
-                value={endDate}
-                onChange={(newValue) => {
-                  setEndDate(newValue);
-                }}
-                renderInput={(params) => (
-                  <TextField {...params} {...register('endDate')} error={!!errors.endDate} />
-                )}
+              <DatePicker
+                label='Set Auction Length'
+                name='endDate'
+                selected={values.endDate}
+                onChange={(d) => onChange(null, 'endDate', d)}
+                error={errors?.endDate}
               />
             </LocalizationProvider>
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">Token</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={token}
-                label="Token"
-                onChange={handleChange}
-              >
-                {Object.keys(tokens).map((t, index) => (
-                  <MenuItem key={`${token}+${index}`} value={t}>
-                    <TokenIcon symbol={tokens[t].icon} />
-                    {tokens[t].symbol}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            <HR color='MID_GREY'/>
-            <Flex>
+            <br/>
+            <HR />
+            <Flex align="center" justify="flex-end" gap={16}>
               <Button onClick={() => handleClose(false)}>Cancel</Button>
-              <Button onClick={handleSubmit(customSubmit)} autoFocus>
+              <Button type='submit'>
                 Start
               </Button>
             </Flex>
@@ -205,5 +207,5 @@ export function StartAuctionModal({ currentToken, open, handleClose }: any) {
         </Container>
       </Modal>
     </div>
-  );
+  )
 }
