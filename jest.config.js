3d9cd54367500067d1613c4a0d52625e6e722d4c
Sebastian Deme <deme.sebastian@gmail.com>
2022-12-12 15:35:40 +0200

    fix(jest): transform ignore patterns for @connect2ic

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,12 @@ module.exports = {
     '\\.(scss|svg|css)$': '<rootDir>/testUtils/fileMock.js',
     '@testUtils': '<rootDir>/testUtils/index.js',
   },
+  testPathIgnorePatterns: ['/node_modules/'],
   moduleDirectories: ['node_modules', 'packages', 'src'],
   testEnvironment: '<rootDir>/testUtils/testEnv.js',
-  transformIgnorePatterns: ['node_modules/(?!@dfinity)'],
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!@connect2ic|@connect2ic/core|@connect2ic/react|@dfinity|event-e3|@astrox)',
+  ],
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest',
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
