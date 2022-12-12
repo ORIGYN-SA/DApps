a64ae8adf629edb034694a511a5c5910d90f88a3
Frank <andreass.frankk@gmail.com>
2022-12-12 19:50:36 +0200

    Update index.tsx

--- packages/apps/wallet/src/pages/Wallet/index.tsx
+++ packages/apps/wallet/src/pages/Wallet/index.tsx
@@ -381,10 +381,10 @@ const WalletPage = () => {
     }
   }
 
-var today = new Date();
-var date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = time+','+' '+date;
+let today = new Date();
+let date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
+let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+let dateTime = time+','+' '+date;
  
 console.log('time4',dateTime)
 
