e89c3974a250cb5ff41849c1e1cc5cf58b0241ac
AleTex <68469149+aletex1994@users.noreply.github.com>
2023-01-25 15:16:27 +0100

    Types

---
+++ packages/candy_editor/src/types/index.ts
@@ -0,0 +1,34 @@
+import type { Principal } from '@dfinity/principal';
+
+export type CandyValue =
+  | { Int: bigint }
+  | { Nat: bigint }
+  | { Empty: null }
+  | { Nat16: number }
+  | { Nat32: number }
+  | { Nat64: bigint }
+  | { Blob: Array<number> }
+  | { Bool: boolean }
+  | { Int8: number }
+  | { Nat8: number }
+  | { Nats: { thawed: Array<bigint> } | { frozen: Array<bigint> } }
+  | { Text: string }
+  | { Bytes: { thawed: Array<number> } | { frozen: Array<number> } }
+  | { Int16: number }
+  | { Int32: number }
+  | { Int64: bigint }
+  | { Option: [] | [CandyValue] }
+  | { Floats: { thawed: Array<number> } | { frozen: Array<number> } }
+  | { Float: number }
+  | { Principal: Principal }
+  | {
+      Array: { thawed: Array<CandyValue> } | { frozen: Array<CandyValue> };
+    }
+  | { Class: Array<Property> };
+
+export interface Property {
+  value: CandyValue;
+  name: string;
+  immutable: boolean;
+}
+
