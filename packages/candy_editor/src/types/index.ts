6910b9be0151d5d0035f309acb4d943666934fa3
AleTex <68469149+aletex1994@users.noreply.github.com>
2023-01-25 17:37:01 +0100

    Fixes

--- packages/candy_editor/src/types/index.ts
+++ packages/candy_editor/src/types/index.ts
@@ -1,33 +1,33 @@
 import type { Principal } from '@dfinity/principal';
 
 export type CandyValue =
-  | { Int: bigint }
-  | { Nat: bigint }
   | { Empty: null }
+  | { Bool: boolean }
+  | { Text: string }
+  | { Nat: bigint }
+  | { Nat8: number }
   | { Nat16: number }
   | { Nat32: number }
   | { Nat64: bigint }
-  | { Blob: Array<number> }
-  | { Bool: boolean }
-  | { Int8: number }
-  | { Nat8: number }
   | { Nats: { thawed: Array<bigint> } | { frozen: Array<bigint> } }
-  | { Text: string }
-  | { Bytes: { thawed: Array<number> } | { frozen: Array<number> } }
+  | { Int: bigint }
+  | { Int8: number }
   | { Int16: number }
   | { Int32: number }
   | { Int64: bigint }
-  | { Option: [] | [CandyValue] }
   | { Floats: { thawed: Array<number> } | { frozen: Array<number> } }
   | { Float: number }
+  | { Blob: Array<number> }
+  | { Bytes: { thawed: Array<number> } | { frozen: Array<number> } }
+  | { Option: [] | [CandyValue] }
   | { Principal: Principal }
   | {
       Array: { thawed: Array<CandyValue> } | { frozen: Array<CandyValue> };
     }
   | { Class: Array<Property> };
+
 export interface Property {
   value: CandyValue;
   name: string;
   immutable: boolean;
 }
-
