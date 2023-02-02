301c6bf7d89e7e5c40ff2a1ab39f5a7b660c34b7
Alessandro Tezza <alessandrotezzavr@gmail.com>
2023-02-02 09:41:37 +0100

    Merge Commit
    Merged-PrivateToPublic since it has latest updates for Principal and Float types

--- packages/candy_editor/src/assets/index.tsx
+++ packages/candy_editor/src/assets/index.tsx
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Flex, Button, HR, Select, Container } from '@origyn-sa/origyn-art-ui';
+import { Card, Flex, Button, HR, Select, Container, Modal } from '@origyn-sa/origyn-art-ui';
 import { FormTypes } from './formTypes';
 import type { Property, CandyClassEditor, CandyClass, CandyType } from '../types';
 import { NOT_SELECTED, SELECT_OPTIONS } from '../constants';
 
 export const CandyDataEditor = () => {
-  const [openForm, setOpenForm] = React.useState(false);
   const [candyType, setCandyType] = useState<CandyType>(NOT_SELECTED);
   const [candyClass, setCandyClass] = useState<CandyClass>({ Class: [] });
+  const [openModal, setOpenModal] = useState<boolean>(false);
+
+  const removePropertyFromCandyClass = (property: Property): void => {
+    const newClass = [...candyClass.Class];
+    const index = newClass.indexOf(property);
+    if (index !== -1) {
+      newClass.splice(index, 1);
+    }
+    setCandyClass({ Class: newClass });
+  };
 
   const candyClassEditor: CandyClassEditor = {
     addPropertyToCandyClass: (property: Property) => {
@@ -16,8 +25,12 @@ export const CandyDataEditor = () => {
     candyType: candyType,
   };
 
-  const handleOpenForm = () => {
-    setOpenForm(!openForm);
+  const displayModal = () => {
+    setOpenModal(true);
+  };
+
+  const closeModal = () => {
+    setOpenModal(false);
   };
 
   const handleSelectChange = (candySelectedType: CandyClass) => {
@@ -34,51 +47,84 @@ export const CandyDataEditor = () => {
       <Container>
         <Flex flexFlow="column" gap={16}>
           <Flex>
-            <Button size="small" btnType="filled" onClick={handleOpenForm}>
+            <Button size="small" btnType="filled" onClick={displayModal}>
               + Add Candy Value
             </Button>
           </Flex>
-          {openForm ? (
-            <>
-              <HR marginTop={8} marginBottom={8} />
-              <Flex flexFlow="column" gap={16}>
-                <Flex>
-                  <Select
-                    inputSize="medium"
-                    label="Select Candy Type"
-                    handleChange={(type) => {
-                      handleSelectChange(type.value);
-                    }}
-                    options={SELECT_OPTIONS.map((option) => ({
-                      label: option.label,
-                      value: option.value,
-                    }))}
-                  />
-                </Flex>
-                <HR marginTop={8} marginBottom={8} />
-                <Flex>
-                  {candyType !== null ? FormTypes[candyType.toString()](candyClassEditor, candyType) : null}
-                </Flex>
-              </Flex>
-            </>
-          ) : null}
           {candyClass.Class.length > 0 ? (
             <>
               <HR marginTop={8} marginBottom={8} />
-              <Flex>
-                <b>Your Candy Class: </b>{' '}
-                <pre>
-                  {JSON.stringify(
-                    candyClass,
-                    (this, (key, value) => (typeof value === 'bigint' ? value.toString() : value)),
-                    2,
-                  )}
-                </pre>
+              <Flex flexFlow="column" gap={16}>
+                {candyClass.Class.map((property, index) => (
+                  <Flex key={index} flexFlow="row" gap={8}>
+                    <Flex>
+                      <p>
+                        <b>Name: </b>
+                        {property.name}
+                      </p>
+                    </Flex>
+                    <Flex>
+                      <p>
+                        <b>Value: </b>
+                        {Object.getOwnPropertyNames(property.value)[0].toString()}
+                      </p>
+                    </Flex>
+                    <Flex>
+                      <p>
+                        <b>Immutable: </b>
+                        {property.immutable.toString()}
+                      </p>
+                    </Flex>
+                    <Flex justify="flex-end" align="flex-end">
+                      <Button
+                        size="small"
+                        btnType="filled"
+                        onClick={() => removePropertyFromCandyClass(property)}
+                      >
+                        Remove
+                      </Button>
+                    </Flex>
+                  </Flex>
+                ))}
               </Flex>
             </>
           ) : null}
         </Flex>
       </Container>
+      <Modal
+        closeModal={closeModal}
+        isOpened={openModal}
+        mode="light"
+        size="md"
+      >
+        <Container padding="16px">
+          <Flex flexFlow="column" gap={16}>
+            <Flex>
+              <Select
+                inputSize="medium"
+                label="Select Candy Type"
+                handleChange={(type) => {
+                  handleSelectChange(type.value);
+                }}
+                options={SELECT_OPTIONS.map((option) => ({
+                  label: option.label,
+                  value: option.value,
+                }))}
+              />
+            </Flex>
+            <HR marginTop={8} marginBottom={8} />
+            <Flex>
+              {candyType !== null
+                ? FormTypes[candyType.toString()](candyClassEditor, candyType)
+                : <>
+                  <Flex>Select a Candy Type</Flex>
+                  <HR marginTop={8} marginBottom={16} />
+                </>}
+            </Flex>
+          </Flex>
+        </Container>
+      </Modal>
     </Card>
+
   );
 };
