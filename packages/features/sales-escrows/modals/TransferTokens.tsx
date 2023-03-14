aa815b6ead30bb9789cbd3be3a189e423021964f
Motokoder <motokoder@tutanota.com>
2023-03-13 19:48:54 -0600

    ENG-1494 transfer token fixes

--- packages/features/sales-escrows/modals/TransferTokens.tsx
+++ packages/features/sales-escrows/modals/TransferTokens.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
+import BigNumber from 'bignumber.js';
 import { Principal } from '@dfinity/principal';
-import { sendTransaction, useTokensContext } from '@dapp/features-tokens-provider';
+import { sendTransaction, Token, useTokensContext } from '@dapp/features-tokens-provider';
 import { Container, Flex, HR, Modal, TextInput, Select, Button } from '@origyn/origyn-art-ui';
 import { LinearProgress } from '@mui/material';
 import * as Yup from 'yup';
@@ -37,26 +38,49 @@ const TransferTokensModal = ({ open, handleClose }: any) => {
   // @ts-ignore
   const [values, setValues] = React.useState<any>(validationSchema.default());
   const [errors, setErrors] = React.useState<any>({});
-  const [displayAmount, setDisplayAmount] = useState('0');
-  const [totalDisplayAmount, setTotalDisplayAmount] = useState('0');
-  const [tokenSymbol, setTokenSymbol] = useState('OGY');
-  const [tokenFee, setTokenFee] = useState('');
+  const [amountDisplay, setAmountDisplay] = useState('0');
+  const [totalDisplay, setTotalDisplay] = useState('0');
+  const [feeDisplay, setFeeDisplay] = useState('');
   const [success, setSuccess] = useState(false);
 
-  const onChange = (e?: any, name?: string, value?: any) => {
-    setErrors({ ...errors, [name || e.target.name]: undefined });
-    setValues({ ...values, [name || e.target.name]: value || e.target.value });
+  const onChange = (name: string, value: any) => {
+    setErrors({ ...errors, [name]: undefined });
+    setValues({ ...values, [name]: value });
+
+    if (name === 'amount') {
+      onAmountChanged(value);
+    } else if (name === 'token') {
+      onTokenChanged(value);
+    }
   };
 
-  const onCurrencyChanged = (name: string, value: string) => {
-    let validationMsg = validateTokenAmount(value, tokens[values.token].decimals);
-    setErrors({ ...errors, [name]: validationMsg });
-    setValues({ ...values, [name]: value });
+  const onAmountChanged = (value: string) => {
+    const token = tokens[values.token];
+    let validationMsg = validateTokenAmount(value, token.decimals);
+    if (validationMsg) {
+      setErrors({ ...errors, amount: validationMsg });
+    } else {
+      const amount = toBigNumber(value || 0);
+      updateTotals(token, amount);
+    }
+  };
+
+  const onTokenChanged = (value: string) => {
+    const token = tokens[value];
+    const amount = toBigNumber(values.amount || 0);
+    updateTotals(token, amount);
+  };
+
+  const updateTotals = (token: Token, amount: BigNumber) => {
+    const fee = toLargerUnit(token.fee, token.decimals);
+    const total = amount.plus(fee);
+    setAmountDisplay(amount.toFixed());
+    setTotalDisplay(total.toFixed());
+    setFeeDisplay(toLargerUnit(token.fee, token.decimals).toFixed());
   };
 
   const sendTrx = async (data) => {
     try {
-      console.log(data);
       setInProcess(true);
 
       const token = tokens[data.token];
@@ -107,6 +131,15 @@ const TransferTokensModal = ({ open, handleClose }: any) => {
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
+
+    // TODO: validate address
+
+    const amount = toBigNumber(values.amount || 0);
+    if (amount.isLessThanOrEqualTo(0)) {
+      setErrors({ ...errors, amount: 'Amount can not be 0' });
+      return;
+    }
+
     validationSchema
       .validate(values, { abortEarly: false })
       .then(() => {
@@ -125,25 +158,13 @@ const TransferTokensModal = ({ open, handleClose }: any) => {
   };
 
   useEffect(() => {
-    //const total = Number(values.amount) + tokens[values.token].fee * 0.00000001;
-    const token = tokens[values.token];
-    const amount = toBigNumber(values.amount || 0);
-    const fee = toLargerUnit(token.fee, token.decimals);
-    const total = amount.plus(fee);
+    setValues({ token: 'OGY', amount: '', memo: '', recipientAddress: '' });
+  }, [open]);
 
-    console.log(
-      `token: ${
-        token.symbol
-      }; amount: ${amount.toFixed()}; fee: ${fee.toFixed()}; total: ${total.toFixed()}`,
-    );
-
-    setDisplayAmount(amount.toFixed());
-    setTotalDisplayAmount(total.toFixed());
-    setTokenFee(toLargerUnit(token.fee, token.decimals).toFixed());
-    setTokenSymbol(token.symbol);
-  }, [values]);
-
-  useEffect(() => {}, []);
+  const tokenOptions = Object.keys(activeTokens).map((standard) => ({
+    value: standard,
+    label: standard,
+  }));
 
   return (
     <div>
@@ -153,7 +174,7 @@ const TransferTokensModal = ({ open, handleClose }: any) => {
             <h2>Success!</h2>
             <br />
             <span>
-              Your transfer of {displayAmount} {tokenSymbol} is complete. Click done to return to
+              Your transfer of {amountDisplay} {values.token} is complete. Click done to return to
               the dashboard.
             </span>
             <br />
@@ -177,22 +198,18 @@ const TransferTokensModal = ({ open, handleClose }: any) => {
               <span>Select token</span>
               <Select
                 placeholder="OGY"
-                handleChange={(option) => {
-                  onChange(null, 'token', option.value);
-                }}
-                options={Object.keys(activeTokens).map((standard) => ({
-                  value: standard,
-                  label: standard,
-                }))}
+                handleChange={(option) => onChange('token', option.value)}
+                options={tokenOptions}
+                selectedOption={tokenOptions.find((opt) => opt.label === values.token)}
               />
               <br />
               <Flex flexFlow="row" justify="space-between">
                 <span>Amount</span>
-                <span id="balance">{tokens[tokenSymbol]?.balance}</span>
+                <span id="balance">{tokens[values.token]?.balance}</span>
               </Flex>
               <TextInput
                 name="amount"
-                onChange={(e) => onCurrencyChanged('amount', e.target.value)}
+                onChange={(e) => onChange('amount', e.target.value)}
                 value={values?.amount}
                 error={errors?.amount}
               />
@@ -200,16 +217,20 @@ const TransferTokensModal = ({ open, handleClose }: any) => {
               <span>Recipient Address</span>
               <TextInput
                 name="recipientAddress"
-                onChange={onChange}
+                onChange={(e) => onChange('recipientAddress', e.target.value)}
                 value={values.recipientAddress}
                 error={errors.recipientAddress}
               />
               <br />
               <span>Memo</span>
-              <TextInput name="memo" value={values.memo} onChange={onChange} />
+              <TextInput
+                name="memo"
+                value={values.memo}
+                onChange={(e) => onChange('memo', e.target.value)}
+              />
               <br />
               <span>Transaction Fee</span>
-              <span style={{ color: 'grey' }}>{`${tokenFee}${' '}${tokenSymbol}`}</span>
+              <span style={{ color: 'grey' }}>{`${feeDisplay}${' '}${values.token}`}</span>
               <br />
             </Flex>
             <br />
@@ -217,14 +238,14 @@ const TransferTokensModal = ({ open, handleClose }: any) => {
             <br />
             <Flex flexFlow="row" align="center" justify="space-between">
               <h6>Total Amount</h6>
-              <span>{totalDisplayAmount}</span>
+              <span>{totalDisplay}</span>
             </Flex>
             <br />
             <HR />
             <br />
             <Flex justify="flex-end">
               <Button btnType="filled" type="submit">
-                Transfer {tokenSymbol}
+                Transfer {values.token}
               </Button>
             </Flex>
           </Container>
