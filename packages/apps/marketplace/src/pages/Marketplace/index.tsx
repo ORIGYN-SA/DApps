402dc396a762e31b1e623717c6063bf65c3f9007
Frank <andreass.frankk@gmail.com>
2023-03-22 15:47:14 +0100

    added fix

--- packages/apps/marketplace/src/pages/Marketplace/index.tsx
+++ packages/apps/marketplace/src/pages/Marketplace/index.tsx
@@ -261,7 +261,7 @@ const Marketplace = () => {
                           </Flex>
                           <br />
                           <ShowMoreBlock btnText="Read More">
-                            <p className="secondary_color">{collectionData?.description}</p>
+                            <p>{collectionData?.description}</p>
                           </ShowMoreBlock>
                           <br />
                           <br />
