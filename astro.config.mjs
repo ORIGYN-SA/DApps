1f983042147cdb76a1364d4a1e7d183ec5b2cd5d
pu0238 <michal.lustyk>
2024-09-04 03:05:35 +0200

    Unify HTML files && clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import { loadEnv } from 'vite';
 import mainConfig from './vite.config';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const PORT = Number(process.env.PUBLIC_DEV_SERVER_PORT || 9000);
+const VALIDATE_PRINCIPAL_RGX = '/-/[a-z0-9]{5}-[a-z0-9]{5}-[a-z0-9]{5}-[a-z0-9]{5}-[a-z0-9]{3}/collection/-';
 
 const mode = process.env.NODE_ENV || 'production';
 const env = loadEnv(mode, process.cwd(), '');
 process.env = { ...process.env, ...env };
 
-const rgx = '/-/[a-z0-9]{5}-[a-z0-9]{5}-[a-z0-9]{5}-[a-z0-9]{5}-[a-z0-9]{3}/collection/-';
 
 export default defineConfig({
   integrations: [react()],
@@ -23,13 +20,14 @@ export default defineConfig({
   vite: {
     ...mainConfig,
     server: {
+      port: PORT,
       proxy: {
-        [`^${rgx}`]: {
-          target: 'http://localhost:9001',
+        [`^${VALIDATE_PRINCIPAL_RGX}`]: {
+          target: `http://localhost:${PORT}`,
           changeOrigin: true,
           rewrite: (path) => {
-            const newPath = path.replace(new RegExp(`^${rgx}`), '');
-            console.log(newPath)
+            const newPath = path.replace(new RegExp(`^${VALIDATE_PRINCIPAL_RGX}`), '');
+            console.log(newPath);
             return newPath;
           },
         },
