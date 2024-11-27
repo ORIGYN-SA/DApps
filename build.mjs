173e0119d393545c8c1ab908b813e1a0daa9f759
Victor Raguin <victor.raguin@live.fr>
2024-11-27 12:21:26 +0100

    👷 : update building script to accept different environment

--- build.mjs
+++ build.mjs
@@ -2,13 +2,15 @@ import { execSync } from 'child_process'
 import fs from 'fs'
 import path from 'path'
 
+const env = process.env.BUILD_ENV || 'production'
+
 try {
+  console.log(`Starting build for environment: ${env}`)
   console.log('Removing the dist directory...')
 
   fs.rmSync(path.resolve('.', 'dist'), { recursive: true, force: true })
 
   console.log('Compiling TypeScript...')
-
   execSync('tsc -b', { stdio: 'inherit' })
 
   const bundlePagesDir = path.resolve('.', 'src', 'bundlePages')
@@ -30,14 +32,12 @@ try {
       throw new Error(`Vite configuration file does not exist at: ${viteConfigPath}`)
     }
 
-    console.log(`Executing vite build with config: ${viteConfigPath}`)
-
-    execSync(`vite build -c "${viteConfigPath}"`, { stdio: 'inherit' })
+    console.log(`Executing vite build with config: ${viteConfigPath} for mode: ${env}`)
+    execSync(`vite build -c "${viteConfigPath}" --mode ${env}`, { stdio: 'inherit' })
   }
 
   console.log('Build completed successfully.')
 } catch (error) {
   console.error('Build failed:', error.message)
-
   process.exit(1)
 }
